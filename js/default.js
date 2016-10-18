3182f10b22b2fb6fb03718dcfc609bbca8db45c8
ajinjing <ajingwsm@gmail.com>
2016-10-18 13:30:33 +0800

    fixed carousel bug,compressed pictures and adjust html to pass W3C validation

--- js/default.js
+++ js/default.js
@@ -2,97 +2,104 @@
 // 导航子菜单
 // 
 // 定义匹配符合条件的class的函数
-function hasClass(obj,cls) {
-	return obj.className.match(new RegExp("(\\s|^)"+cls+"(\\s|$)"));
+function hasClass(obj, cls) {
+    return obj.className.match(new RegExp("(\\s|^)" + cls + "(\\s|$)"));
 }
 // 定义增加class的函数
-function addClass(obj,cls) {
-	if (!hasClass(obj,cls)) {
-		obj.className += " "+cls;
-		obj.className = obj.className.trim();
-	}
+function addClass(obj, cls) {
+    if (!hasClass(obj, cls)) {
+        obj.className += " " + cls;
+        obj.className = obj.className.trim();
+    }
 }
 // 定义删除class的函数
-function removeClass(obj,cls){
-	if (hasClass(obj,cls)) {
-		var reg = new RegExp("(\\s|^)"+cls+"(\\s|$)");
-		obj.className = obj.className.replace(reg," ").trim();
-	}
+function removeClass(obj, cls) {
+    if (hasClass(obj, cls)) {
+        var reg = new RegExp("(\\s|^)" + cls + "(\\s|$)");
+        obj.className = obj.className.replace(reg, " ").trim();
+    }
 }
 // 
 // 轮播图carousel
 // 
-function carousel(wrap,inner,spanlist) {
-	var index = 0; 
-	var distance = wrap.offsetWidth;
-	var time;
-	// 清除轮播图中的所有下标按钮的颜色
-	function clear(){
-		for (var i = 0; i < spanlist.length; i++) {
-			spanlist[i].className = "";
-		}
-	}
-	// 定义轮播图中图片滑动的函数
-	function AutoGo() {
-		var start = inner.offsetLeft;
-		var end = index*distance*(-1);
-		var change = end - start;
-		// 用计时器为图片添加动画效果
-		var timer;
-		var t = 0;
-		var maxT = 30;
-		clear();
-		if (index === spanlist.length) {
-			spanlist[0].className = "active_select";
-		}
-		else {
-			spanlist[index].className = "active_select";
-		}
-		clearInterval(timer);
-		timer = setInterval(function(){
-			t++;
-			if(t>=maxT){
-				clearInterval(timer);
-			}
-			inner.style.left = change/maxT*t+start+"px";
-			if (index === spanlist.length && t>=maxT) {
-				inner.style.left = 0;
-				index = 0;
-			}
-		},15);
-	}
-	// 定义图片向右滑动的函数
-	function forward(){
-		index++;
-		if (index>spanlist.length) {
-			index = 0;
-		}
-		AutoGo();
-	}
-	// 定义图片向左滑动的函数
-	function backward(){
-		index--;
-		if (index<0) {
-			index = spanlist.length - 1;
-			inner.style.left = (index+1)*distance*(-1)+"px";
-		}
-		AutoGo();
-	}
-	// 开启图片自动向右滑动的计时器
-	time = setInterval(forward,3000);
-	// 设置鼠标悬停时动画停止
-	wrap.onmouseover = function(){
-		clearInterval(time);
-	};
-	wrap.onmouseout = function(){
-		time = setInterval(forward,3000);
-	};
-	// 遍历每个下标按钮让其切换到对应的图片
-	for (var i = 0; i < spanlist.length; i++) {
-		spanlist[i].onclick = function(){
-			index = this.innerText - 1;
-			AutoGo();
-		};
-	}
+function carousel(wrap, inner, spanlist) {
+    var index = 0;
+    var distance = wrap.offsetWidth;
+    var time;
+    // 清除轮播图中的所有下标按钮的颜色
+    function clear() {
+        for (var i = 0; i < spanlist.length; i++) {
+            spanlist[i].className = "";
+        }
+    }
+    // 定义轮播图中图片滑动的函数
+    function AutoGo() {
+        var start = inner.offsetLeft;
+        var end = index * distance * (-1);
+        var change = end - start;
+        // 用计时器为图片添加动画效果
+        var timer;
+        var t = 0;
+        var maxT = 30;
+        clear();
+        if (index === spanlist.length) {
+            spanlist[0].className = "active_select";
+        } else {
+            spanlist[index].className = "active_select";
+        }
+        clearInterval(timer);
+        timer = setInterval(function() {
+            t++;
+            if (t >= maxT) {
+                clearInterval(timer);
+            }
+            inner.style.left = change / maxT * t + start + "px";
+            if (index === spanlist.length && t >= maxT) {
+                inner.style.left = 0;
+                index = 0;
+            }
+        }, 15);
+    }
+    // 定义图片向右滑动的函数
+    function forward() {
+        index++;
+        if (index > spanlist.length) {
+            index = 0;
+        }
+        AutoGo();
+    }
+    // 定义图片向左滑动的函数
+    function backward() {
+        index--;
+        if (index < 0) {
+            index = spanlist.length - 1;
+            inner.style.left = (index + 1) * distance * (-1) + "px";
+        }
+        AutoGo();
+    }
+    // 开启图片自动向右滑动的计时器
+    time = setInterval(forward, 3000);
+    // 设置鼠标悬停时动画停止
+    wrap.addEventListener('mouseenter', function() {
+        clearInterval(time);
+    }, false);
+    wrap.addEventListener('mouseleave', function() {
+        clearInterval(time);
+        time = setInterval(forward, 3000);
+    }, false);
+    // 只有激活当前窗口才开始计时
+    window.addEventListener('blur', function() {
+        clearInterval(time);
+    }, false);
+    window.addEventListener('focus', function() {
+        clearInterval(time);
+        time = setInterval(forward, 3000);
+    }, false);
+    // 遍历每个下标按钮让其切换到对应的图片
+    for (var i = 0; i < spanlist.length; i++) {
+        spanlist[i].onclick = function() {
+            index = this.innerText - 1;
+            AutoGo();
+        };
+    }
 }
-
